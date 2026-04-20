8e0818ed40d8d4c9993839301e5c7337df5afeeb
omka <omorbekteam@gmail.com>
2026-04-20 11:57:03 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { useState } from 'react';
-import './App.css';
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  return <h1>Hello World</h1>;
+}
 
 export default App;
